9e3447b0ebc7f079dc350558005c5652e2ab8c32
roilanrodriguez55 <roilanrodriguez55@gmail.com>
2024-01-07 00:24:40 -0500

    Correction event click button

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-const switchButton = document.querySelector(".switch-container");
+const switchButton = document.querySelector(".switch-container button");
 const iconBar = document.querySelector(".icon-bar");
 
 switchButton.addEventListener("click", (e) => {
   e.preventDefault();
+  e.stopPropagation();
   iconBar.classList.toggle("vertical");
   iconBar.classList.toggle("horizontal");
 });
